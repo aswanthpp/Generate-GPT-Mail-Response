74e04dd9ef11c9d496e56f09b656e417cb8b7647
Z0044J2 <aswanth.p@target.com>
2023-09-22 12:59:37 +0530

    added popup enable function

--- content.js
+++ content.js
@@ -15,15 +15,78 @@ function getGptKeyFromLocalStorage() {
     }
   });
 }
-function createInnerHTML(){
-        const form = document.createElement('form');
-        form.innerHTML = `
-          <label for="prompt">Create Prompt?</label><br>
-          <input type="text" id="textPrompt" name="textPrompt"><br>
-          <input type="submit" value="Submit">
+function getPopupHTML(){
+        const form = `
+          <div>
+            <form>
+            <textarea rows="4" cols="50" id="textPrompt" name="textPrompt" placeholder="Enter prompt here"></textarea>
+            <br>
+            <input type="submit" id="submitBtn" value="Submit">
+            </form>
+          </div>
         `;
         return form;
 }
+
+function showPopup() {
+  // Define CSS styles for the popup background and content
+  var popupStyles = `
+      .popup-background {
+          display: none;
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background-color: rgba(0, 0, 0, 0.7);
+      }
+
+      .popup-content {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          background-color: #fff;
+          padding: 20px;
+          border-radius: 5px;
+          box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+      }
+  `;
+
+  // Create a <style> element and set the styles
+  var styleElement = document.createElement('style');
+  styleElement.textContent = popupStyles;
+
+  // Append the <style> element to the document's head
+  document.head.appendChild(styleElement);
+
+  // Create the popup background
+  var popupBackground = document.createElement('div');
+  popupBackground.className = 'popup-background';
+
+  // Create the popup content
+  var popupContent = document.createElement('div');
+  popupContent.className = 'popup-content';
+  popupContent.innerHTML = getPopupHTML();
+
+  // Append the popup content to the background
+  popupBackground.appendChild(popupContent);
+
+  // Append the background to the body
+  document.body.appendChild(popupBackground);
+
+  // Add an event listener to close the popup
+  // var closePopupBtn = document.getElementById('submitBtn');
+  // closePopupBtn.addEventListener('click', function() {
+  //     document.body.removeChild(popupBackground);
+  // });
+
+  // Display the popup
+  popupBackground.style.display = 'block';
+  return document;
+}
+
+
 async function generateText(prompt,API_KEY) {
     console.log("Calling Open AI Completion API");
     const url = 'https://api.openai.com/v1/completions';
@@ -67,16 +130,20 @@ InboxSDK.load(2, SDK_KEY).then((sdk) => {
       onClick: function(event) {
           console.log("Compose inside compose view");
           getGptKeyFromLocalStorage();
-          const form = createInnerHTML();
+          // const form = createInnerHTML();
 
         // Add the form to the compose view
-        event.composeView.insertHTMLIntoBodyAtCursor(form);
+        // event.composeView.insertHTMLIntoBodyAtCursor(form);
+        const document=showPopup();
+        event.composeView.insertHTMLIntoBodyAtCursor(document);
+
 
         // Add a submit event listener to the form
-        form.addEventListener('submit', async (e) => {
+        document.addEventListener('submit', async (e) => {
+          document.body.removeChild(popupBackground);
           e.preventDefault();
           // Get the value of the prompt input field
-          const textPrompt = form.querySelector('#textPrompt').value;
+          const textPrompt = document.querySelector('#textPrompt').value;
           console.log("Compose Email for : "+textPrompt);
           const responseText = await generateText(textPrompt,API_KEY);
           if(event.composeView.isReply){
